f01a8d9ff0c10d78a6652f27be98205ef6f5cdcd
Simon Li <spli@dundee.ac.uk>
2017-05-02 11:36:04 +0100

    Optionall yread  screen-file in map_series.py

--- scripts/omero/map_series.py
+++ scripts/omero/map_series.py
@@ -27,7 +27,9 @@ corresponding well, field and image id.
 
 import sys
 import argparse
+import csv
 import getpass
+import os
 from string import uppercase as LETTERS
 from operator import itemgetter
 
@@ -39,12 +41,23 @@ DEFAULT_USER = getpass.getuser()
 def make_parser():
     parser = argparse.ArgumentParser(description=__doc__)
     parser.add_argument('screen_id', metavar="SCREEN_ID", type=int)
+    parser.add_argument('--screen-file', metavar="SCREEN_FILE")
     parser.add_argument("-H", "--host", metavar="HOST", default="localhost")
     parser.add_argument("-P", "--port", metavar="PORT", default=4064)
     parser.add_argument("-U", "--user", metavar="USER", default=DEFAULT_USER)
     parser.add_argument("-G", "--group", metavar="GROUP")
     parser.add_argument("-o", "--out-file", metavar="FILE", help="output file")
     return parser
+
+
+def load_screen_file(fname):
+    name_filepath_map = {}
+    with open(fname) as f:
+        for r in csv.reader(f, delimiter='\t'):
+            if len(r) != 2:
+                raise Exception('Expected 2 items, found %d: %s' % (len(r), r))
+            name_filepath_map[r[0]] = os.path.basename(r[1])
+    return name_filepath_map
 
 
 def main(argv):
@@ -60,6 +73,11 @@ def main(argv):
     screen = conn.getObject("Screen", args.screen_id)
     print "writing to %s" % args.out_file
     print "SCREEN: %s" % screen.name
+
+    name_filepath_map = None
+    if args.screen_file:
+        name_filepath_map = load_screen_file(args.screen_file)
+
     with open(args.out_file, "w") as fo:
         fo.write("PLATE\tSERIES\tWELL\tFIELD\tImageID\tWellID\n")
         for p in screen.listChildren():
@@ -70,8 +88,12 @@ def main(argv):
                 for i in xrange(n_fields):
                     img = w.getImage(i)
                     well_tag = "%s%02d" % (LETTERS[w.row], w.column + 1)
+                    if name_filepath_map:
+                        pname = name_filepath_map[p.name]
+                    else:
+                        pname = p.name
                     rows.append((
-                        p.name, img.getSeries(), well_tag, i + 1, img.id, w.id
+                        pname, img.getSeries(), well_tag, i + 1, img.id, w.id
                     ))
             rows.sort(key=itemgetter(1))
             rows.sort()
